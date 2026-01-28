9f99c1bc5ccb5d00ab9331cbec968f14671dcd1b
ZRegistani <zregistani@gmail.com>
2026-01-28 10:27:23 -0500

    build: clean pom and fix waits imports for CI

--- src/main/java/utils/Waits.java
+++ src/main/java/utils/Waits.java
@@ -5,8 +5,10 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class Waits {
 
